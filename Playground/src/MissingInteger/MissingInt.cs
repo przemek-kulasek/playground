dd17ea78fb5921326d898ddffb91d460747d9890
przemyslaw.kulasek <przemyslaw.kulasek@bluesoft.com>
2023-09-05 13:04:10 +0200

    Codility: Missing Integer

---
+++ Playground/src/MissingInteger/MissingInt.cs
@@ -0,0 +1,28 @@
+﻿namespace MissingInteger;
+
+public static class MissingInt
+{
+    public static int Find(int[] A)
+    {
+        var n = A.Length;
+        var seen = new bool[n + 1];
+
+        foreach (var num in A)
+        {
+            if (num > 0 && num <= n)
+            {
+                seen[num] = true;
+            }
+        }
+        
+        for (var i = 1; i <= n; i++)
+        {
+            if (!seen[i])
+            {
+                return i;
+            }
+        }
+
+        return n + 1;
+    }
+}
---
+++ Playground/src/MissingInteger/Program.cs
@@ -0,0 +1,6 @@
+﻿using MissingInteger;
+
+var testData = new int[] { 1, 3, 6, 4, 1, 2 };
+var result = MissingInt.Find(testData);
+
+Console.WriteLine($"Missing integer is {result}");
