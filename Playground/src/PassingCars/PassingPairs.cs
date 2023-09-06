bacf5df9089f1f2a8006b1c5b48d68c387e9efaa
przemyslaw.kulasek <przemyslaw.kulasek@bluesoft.com>
2023-09-06 08:07:42 +0200

    Codility: Passing Cars

---
+++ Playground/src/PassingCars/PassingPairs.cs
@@ -0,0 +1,30 @@
+﻿namespace PassingCars;
+
+public static class PassingPairs
+{
+    
+    public static int Count(int[] A)
+    {
+        var countEast = 0;
+        var passingCount = 0;
+        
+        foreach (var car in A)
+        {
+            if (car == 0)
+            {
+                countEast++;
+            }
+            else
+            {
+                passingCount += countEast;
+                
+                if (passingCount > 1000000000)
+                {
+                    return -1;
+                }
+            }
+        }
+
+        return passingCount;
+    }
+}
---
+++ Playground/src/PassingCars/Program.cs
@@ -0,0 +1,6 @@
+﻿using PassingCars;
+
+var testData = new int[] { 0, 1, 0, 1, 1 };
+var result = PassingPairs.Count(testData);
+
+Console.WriteLine($"Number of passing pairs is {result}");
